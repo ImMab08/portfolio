f70b5a5de56bb87cdcd4ce9ca77cace0d705cf1c
mab08 <frankyvargas15@gmail.com>
2024-07-10 00:28:38 -0500

    Actualización de Idiomas

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -23,7 +23,7 @@ const Hero = () => {
       <div className='lg:flex lg:w-full lg:px-32'>
 
         <div className='lg:w-1/2 lg:flex lg:flex-col lg:justify-center'>
-          <h1 className='text-tertiary text-2xl lg:text-5xl font-bold leading-[2px]'>{t('title')}</h1>
+          <h1 className='text-tertiary text-2xl lg:text-5xl font-bold leading-6 md:leading-[2px]'>{t('title')}</h1>
           <h2 className='text-tertiary text-4xl lg:text-6xl font-bold'>{t('subtitle')}</h2>
           <h1 className='text-tertiary text-2xl lg:text-5xl font-bold'>
             <span className='bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text h-14'>
@@ -34,7 +34,7 @@ const Hero = () => {
           <div className='flex items-center mt-5 lg:mt-14'>            
             <a download="" href="/HVFrankyHelyVargasLopez.pdf" target="_blank" className="flex mr-4 rounded-lg bg-white text-dark p-1 lg:p-2 px-4 lg:px-6 text-base lg:text-lg font-semibold">{t('cv')}
               <div className="w-[20px]">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="w-full h-auto  ml-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="w-full h-auto ml-1">
                   <path fill="none" d="M0 0h24v24H0z"></path>
                   <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-5m-7 1L20 4m-5 0h5v5"></path>
                 </svg>
